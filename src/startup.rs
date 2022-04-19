9fa24ea055f6c37cabac745056bfa0c856a66e95
richardjypark <richard@richardjypark.com>
2022-04-19 18:54:41 -0400

    request id logging

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,15 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    // wrap pool with `web::Data` an Arc smart pointer
     let db_pool = web::Data::new(db_pool);
-    // capture `connection' from surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // get pointer copy and attach to application state
